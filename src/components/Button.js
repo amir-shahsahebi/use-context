c71bd2d4b711347f3c13086444dd16891f883920
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-12 12:55:27 +0430

    context.Consumer added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
-import { useContext } from "react";
-import languageContext from "../contexts/languageContext";
+// import { useContext } from "react";
+import LanguageContext from "../contexts/languageContext";
 const Button = () => {
-  const context = useContext(languageContext);
-  const text = context === "english" ? "Submit" : "Voorleggen";
-  return <button className="ui button primary">{text}</button>;
+  // const context = useContext(languageContext);
+  // const text = context === "english" ? "Submit" : "Voorleggen";
+  // return <button className="ui button primary">{text}</button>;
+  return (
+    <button className="ui button primary">
+      <LanguageContext.Consumer>
+        {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+      </LanguageContext.Consumer>
+    </button>
+  );
 };
 
 export default Button;
